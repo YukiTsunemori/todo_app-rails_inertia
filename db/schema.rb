72f01dc5c590124c2f84697ed495b03846efa60b
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-12-04 18:04:35 +0900

    タスクモデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_08_01_153828) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_03_135623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,6 +21,17 @@ ActiveRecord::Schema[8.1].define(version: 2025_08_01_153828) do
     t.string "user_agent"
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.datetime "completed_at"
+    t.datetime "created_at", null: false
+    t.datetime "deadline_at"
+    t.text "memo"
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +45,5 @@ ActiveRecord::Schema[8.1].define(version: 2025_08_01_153828) do
   end
 
   add_foreign_key "sessions", "users"
+  add_foreign_key "tasks", "users"
 end
