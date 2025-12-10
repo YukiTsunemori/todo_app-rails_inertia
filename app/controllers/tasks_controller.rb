ad4578ad1b2e8c05e6140e8e5526714854817f02
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-12-10 19:32:06 +0900

    Taskの新規作成と一覧機能を追加

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class TasksController < ApplicationController
+  def index
+    tasks = Current.user.tasks.all
+    render inertia: "Tasks/Index", props: {
+      tasks: tasks
+    }
+  end
+
+  def create
+    task = Current.user.tasks.build(task_params)
+
+    if task.save
+      redirect_to root_path
+    else
+      redirect_to root_path, inertia: {errors: task.errors}
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :memo, :deadline_at)
+  end
+end
